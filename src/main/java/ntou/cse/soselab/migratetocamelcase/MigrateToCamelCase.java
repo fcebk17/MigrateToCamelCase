1450754de63d28a7451b313b9b896d721598641c
Tzuyu <fcebk17@gmail.com>
2024-08-09 09:55:22 +0800

    feat: fail Identifier transfer

--- src/main/java/ntou/cse/soselab/migratetocamelcase/MigrateToCamelCase.java
+++ src/main/java/ntou/cse/soselab/migratetocamelcase/MigrateToCamelCase.java
@@ -6,8 +6,7 @@ import org.openrewrite.java.TreeVisitingPrinter;
 import org.openrewrite.java.tree.J;
 import com.google.common.base.CaseFormat;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MigrateToCamelCase extends Recipe {
 
@@ -24,69 +23,69 @@ public class MigrateToCamelCase extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new MigrateToCamelCaseVisitor();
+        return new JavaIsoVisitor<>() {
+            @Override
+            public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations variableDeclarations, ExecutionContext executionContext) {
+                J.VariableDeclarations v = super.visitVariableDeclarations(variableDeclarations, executionContext);
+                List<J.VariableDeclarations.NamedVariable> updateVariables = new ArrayList<>(v.getVariables());
+
+                for (J.VariableDeclarations.NamedVariable variable :v.getVariables()) {
+                    String name = variable.getSimpleName();
+
+                    if (!isCamelCase(name)) {
+                        name = toCamelCase(name);
+                    }
+
+                    J.Identifier i = variable.getName().withSimpleName(name);
+
+                    updateVariables.add(variable.withName(i));
+
+                }
+
+
+                if (getCursor().getMessage("change") != null) {
+                    return v.withVariables(updateVariables);
+                }
+
+
+                return v.withVariables(updateVariables);
+            }
+
+            @Override
+            public  J.VariableDeclarations.NamedVariable visitVariable(J.VariableDeclarations.NamedVariable ident, ExecutionContext executionContext) {
+                getCursor().putMessage("change", 1);
+                return super.visitVariable(ident, executionContext);
+            }
+
+
+        };
     }
 
     // JavaIsoVisitor 可以產生與 input 相同的樹，可以對 source code 進行更改
     // ExecutionContext 可在 Visitor 運行中傳遞與共享訊息
-    public class MigrateToCamelCaseVisitor extends JavaIsoVisitor<ExecutionContext> {
+    public boolean isCamelCase(String name) {
+        String camelCasePattern = "([a-z]+[A-Z]+\\w+)+";
+        return name.matches(camelCasePattern);
+    }
 
-        @Override
-        public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations varDecl, ExecutionContext executionContext) {
-            J.VariableDeclarations v = super.visitVariableDeclarations(varDecl, executionContext);
-            List<J.VariableDeclarations.NamedVariable> updateVariables = new ArrayList<>();
+    public boolean isUpperCamel(String name) {
+        String upperCamelPattern = "([A-Z][a-z0-9]+)+";
+        return name.matches(upperCamelPattern);
+    }
 
-            for (J.VariableDeclarations.NamedVariable variable : varDecl.getVariables()) {
-                J.Identifier identifierName = variable.getName(); // 取得 J.Identifier
+    public String toCamelCase(String name) {
+        String converted;
 
-                String newName = identifierName.getSimpleName();  // 取得 J.Identifier 的名字
-
-                if (!isCamelCase(newName)) {
-                    newName = toCamelCase(newName);
-                }
-
-                J.Identifier updateIdeName = identifierName.withSimpleName(newName)
-                                                           .withPrefix(identifierName.getPrefix())
-                                                           .withMarkers(identifierName.getMarkers());
-
-                J.VariableDeclarations.NamedVariable updateVariable = variable.withName(updateIdeName)
-                                                                              .withPrefix(variable.getPrefix())
-                                                                              .withMarkers(variable.getMarkers())
-                                                                              .withType(variable.getType());
-
-                updateVariables.add(updateVariable);
-            }
-
-            J.VariableDeclarations updateVariableDeclarations = v.withVariables(updateVariables);
-
-            return updateVariableDeclarations;
+        if (name.contains("-")) {
+            converted = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, name);
+        } else if (name.contains("_")) {
+            converted = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name);
+        } else if (isUpperCamel(name)) {
+            converted = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, name);
+        } else {
+            converted = name;
         }
 
-        private boolean isCamelCase(String name) {
-            String camelCasePattern = "([a-z]+[A-Z]+\\w+)+";
-            return name.matches(camelCasePattern);
-        }
-
-        private boolean isUpperCamel(String name) {
-            String upperCamelPattern = "([A-Z][a-z0-9]+)+";
-            return name.matches(upperCamelPattern);
-        }
-
-        private String toCamelCase(String name) {
-            String converted;
-
-            if (name.contains("-")) {
-                converted = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, name);
-            } else if (name.contains("_")) {
-                converted = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name);
-            } else if (isUpperCamel(name)) {
-                converted = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, name);
-            } else {
-                converted = name;
-            }
-
-            return converted;
-        }
-
+        return converted;
     }
 }
--- src/test/java/ntou/cse/soselab/migratetocamelcase/MigrateToCamelCaseTest.java
+++ src/test/java/ntou/cse/soselab/migratetocamelcase/MigrateToCamelCaseTest.java
@@ -1,8 +1,10 @@
 package ntou.cse.soselab.migratetocamelcase;
 
 import org.junit.jupiter.api.Test;
-import org.openrewrite.test.*;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.openrewrite.java.Assertions.java;
 
 class MigrateToCamelCaseTest implements RewriteTest {
@@ -10,32 +12,31 @@ class MigrateToCamelCaseTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipe(new MigrateToCamelCase());  // 設置要使用的 Recipe
-        // 可以使用 parser
     }
+
     @Test
-    void testCamelCase() {
+    void forTest() {
         rewriteRun(
-                java( //groovy
+                java(
                         """
                                 class A {
-                                    public void test() {
-                                        String hello_World = "hello";
-                                        System.out.println("1");
-                                    }
+                                    int test_case() {
+                                        int hello_world = 21;
+                                        return 1;
+                                    } 
                                 }
                                 """,
                         """
                                 class A {
-                                    public void test() {
-                                        String helloWorld = "hello";
-                                        System.out.println("1");
-                                    }
+                                    int testCase() {
+                                        int helloWorld = 21;
+                                        return 1;
+                                    } 
                                 }
                                 """
                 )
         );
     }
-
 
 
 }
